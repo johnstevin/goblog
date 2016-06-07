5704491e9184e0a0309dfa530aa56d173e209c4d
stevin <rongzhenbang@medlinker.net>
2016-06-07 17:10:14 +0800

    modify

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 import (
-	//"github.com/astaxie/beego"
+	"github.com/astaxie/beego"
 	"github.com/johnstevin/goblog/app/models"
+	"net/http"
+	"html/template"
 )
 
+const VERSION = "1.0.0"
+
 func main() {
+	beego.ErrorHandler("404", page_not_found)
+	beego.ErrorHandler("dbError", dbError)
+	
 	models.Init()
+	beego.AppConfig.Set("version", VERSION)
+	beego.Run()
+}
+
+func page_not_found(rw http.ResponseWriter, r *http.Request){
+    t,_:= template.New("404.html").ParseFiles(beego.BConfig.WebConfig.ViewsPath + "/error/404.html")
+    data := make(map[string]interface{})
+    data["content"] = "page not found"
+    t.Execute(rw, data)
+}
+
+func dbError(rw http.ResponseWriter, r *http.Request){
+    t,_:= template.New("dberror.html").ParseFiles(beego.BConfig.WebConfig.ViewsPath + "/error/dberror.html")
+    data :=make(map[string]interface{})
+    data["content"] = "database is now down"
+    t.Execute(rw, data)
 }
